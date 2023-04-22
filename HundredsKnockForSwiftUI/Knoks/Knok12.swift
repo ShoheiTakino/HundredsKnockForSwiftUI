4cb71398438345f801ff6ca09867d1cc3bf01473
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-23 01:28:20 +0900

    Listを使用して、遷移先の戻るボタンを非表示のする

---
+++ HundredsKnockForSwiftUI/Knoks/Knok12.swift
@@ -0,0 +1,61 @@
+//
+//  Knok12.swift
+//  HundredsKnockForSwiftUI
+//
+//  Created by 滝野翔平 on 2023/04/23.
+//
+
+import SwiftUI
+
+struct Knok12: View {
+    var body: some View {
+        NavigationStack {
+            List {
+                Section {
+                    NavigationLink {
+                        DetaileView(text: "Apple")
+                            .navigationBarBackButtonHidden()
+                    } label: {
+                        Text("Apple")
+                    }
+                    NavigationLink {
+                        DetaileView(text: "SwiftUI")
+                    } label: {
+                        Text("SwiftUI")
+                    }
+                } header: {
+                    Text("デフォルト表示")
+                }
+
+                Section {
+                    Text("Apple")
+                        .listRowSeparator(.hidden)
+                    Text("SwiftUI")
+                } header: {
+                    Text("セパレータ無し")
+                }
+                .listSectionSeparator(.hidden)
+            }
+        }
+    }
+}
+
+private struct DetaileView: View {
+    @State private var text: String
+
+    init(text: String) {
+        self.text = text
+    }
+
+    var body: some View {
+        Text(text)
+    }
+}
+
+// 模範解答なし
+
+struct Knok12_Previews: PreviewProvider {
+    static var previews: some View {
+        Knok12()
+    }
+}
