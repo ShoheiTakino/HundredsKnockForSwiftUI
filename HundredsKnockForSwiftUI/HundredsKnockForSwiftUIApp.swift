d7c21cbbb5b63d0968d465ac3ce4fcf7e55f7eb0
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-22 22:40:53 +0900

    Initial Commit

---
+++ HundredsKnockForSwiftUI/HundredsKnockForSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HundredsKnockForSwiftUIApp.swift
+//  HundredsKnockForSwiftUI
+//
+//  Created by 滝野翔平 on 2023/04/22.
+//
+
+import SwiftUI
+
+@main
+struct HundredsKnockForSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
