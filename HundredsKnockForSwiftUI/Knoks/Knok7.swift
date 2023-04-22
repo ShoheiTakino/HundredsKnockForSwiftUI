1aeac0123b160603ddda65b21aad5312d6d6e6c9
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-23 01:27:17 +0900

    pickerを表示する

---
+++ HundredsKnockForSwiftUI/Knoks/Knok7.swift
@@ -0,0 +1,58 @@
+//
+//  Knok7.swift
+//  HundredsKnockForSwiftUI
+//
+//  Created by 滝野翔平 on 2023/04/22.
+//
+
+import SwiftUI
+
+struct Knok7: View {
+    @State private var selection = 3
+    private let list = ["みかん",
+                        "ぶどう",
+                        "りんご",
+                        "バナナ",
+                        "もも"]
+
+    var body: some View {
+        HStack {
+            Text("選択値：\(list[selection])")
+            Picker(selection: $selection, label:
+                Text("フルーツを選択")) {
+                ForEach(0..<list.count) { index in
+                    Text(list[index]).tag(index)
+                }
+            }.pickerStyle(.wheel)
+            .frame(width: 200)
+        }
+    }
+}
+
+
+//struct Knok7: View {
+//    var pokemons = ["Snorlax", "Pikachu", "Slowpoke", "Meowth"]
+//    @State private var selectedPokemon = 0
+//
+//    var body: some View {
+//        HStack {
+//            Spacer()
+//            Text(pokemons[selectedPokemon])
+//            Picker("Pokemon", selection: $selectedPokemon) {
+//                ForEach(0 ..< pokemons.count) {
+//                    Text(self.pokemons[$0])
+//                }
+//            }.pickerStyle(WheelPickerStyle())
+//                .onReceive([self.selectedPokemon].publisher.first()) { (value) in
+//                    print("selectedPokemon: \(value)")
+//                    print(self.pokemons[value])
+//            }
+//        }
+//    }
+//}
+
+struct Knok7_Previews: PreviewProvider {
+    static var previews: some View {
+        Knok7()
+    }
+}
