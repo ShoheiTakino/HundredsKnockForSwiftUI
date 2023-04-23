ed94717a66b2034f451e1dfa1960f27ef0cda161
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-23 14:10:01 +0900

    viewModefiler内か

---
+++ HundredsKnockForSwiftUI/Utils/CustomTextModifier.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTextModifier.swift
+//  HundredsKnockForSwiftUI
+//
+//  Created by 滝野翔平 on 2023/04/23.
+//
+
+import SwiftUI
+
+struct CustomTextModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 24, weight: .bold, design: .rounded))
+            .foregroundColor(.red)
+    }
+}
