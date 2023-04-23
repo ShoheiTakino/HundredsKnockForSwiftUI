6b7c50d5da7cec534390c0589699ac097983b73e
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-23 14:09:49 +0900

    Viewのエクステンション追加

---
+++ HundredsKnockForSwiftUI/Extentions/View+.swift
@@ -0,0 +1,15 @@
+//
+//  View+.swift
+//  HundredsKnockForSwiftUI
+//
+//  Created by 滝野翔平 on 2023/04/23.
+//
+
+import SwiftUI
+
+extension View {
+    
+    func customTextStyle() -> some View {
+        self.modifier(CustomTextModifier())
+    }
+}
