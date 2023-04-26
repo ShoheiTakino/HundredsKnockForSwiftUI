890ed2e801c45f9361b61a96b635f80cf20a26bb
ShoheiTakino <shohei0522penguin@gmail.com>
2023-04-26 22:09:45 +0900

    画面横むうき

---
+++ HundredsKnockForSwiftUI/Knoks/Knok33.swift
@@ -0,0 +1,20 @@
+//
+//  Knok33.swift
+//  HundredsKnockForSwiftUI
+//
+//  Created by 滝野翔平 on 2023/04/26.
+//
+
+import SwiftUI
+
+struct Knok33: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Knok33_Previews: PreviewProvider {
+    static var previews: some View {
+        Knok33()
+    }
+}
