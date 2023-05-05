83f17f81c8c3e86a4208b51d80bafbba3c437e2f
ShoheiTakino <shohei0522penguin@gmail.com>
2023-05-05 20:37:20 +0900

    ファイル追加

---
+++ HundredsKnockForSwiftUI/Knoks/Knok56.swift
@@ -0,0 +1,20 @@
+//
+//  Knok56.swift
+//  HundredsKnockForSwiftUI
+//
+//  Created by 滝野翔平 on 2023/05/02.
+//
+
+import SwiftUI
+
+struct Knok56: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Knok56_Previews: PreviewProvider {
+    static var previews: some View {
+        Knok56()
+    }
+}
